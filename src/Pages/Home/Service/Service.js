f7b4d55596297b67079d3c81f8b9bc7af9950b5b
abdurrazzakhera <hhera1413@gmail.com>
2022-04-17 13:53:05 +0600

    card design

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,19 +1,29 @@
 import React from "react";
+import { Card, Col, ListGroup, ListGroupItem } from "react-bootstrap";
 
 const Service = ({ service }) => {
-  const { name, price, duration, packageincludes } = service;
+  const { name, price, duration, packageincludes, image } = service;
   return (
-    <div>
-      <h1>Name : {name}</h1>
-      <p>Price : {price}</p>
-      <span>Duration : {duration}</span>
-      {packageincludes.map((pakege) => (
-        <li>{pakege}</li>
-      ))}
-      <div>
-        <button className='btn btn-secondary'>Check Out</button>
-      </div>
-    </div>
+    <Col style={{ height: "700px" }}>
+      <Card>
+        <Card.Img variant='top' src={image} />
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <Card.Text> Duration : {duration}</Card.Text>
+          <ListGroup className='list-group-flush'>
+            {packageincludes.map((pakage) => (
+              <ListGroupItem>
+                <li>{pakage} </li>
+              </ListGroupItem>
+            ))}
+          </ListGroup>
+          <Card.Text> Price : {price}</Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <button className='btn btn-secondary w-100 '>Check Out</button>
+        </Card.Footer>
+      </Card>
+    </Col>
   );
 };
 
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Row } from "react-bootstrap";
 import useServices from "../../../Hooks/useServices";
 import Service from "../Service/Service";
 
@@ -6,10 +7,12 @@ const Services = () => {
   const [services, setServices] = useServices();
   return (
     <div>
-      <h1>This is Services : {services.length}</h1>
-      {services.map((service) => (
-        <Service key={service.id} service={service}></Service>
-      ))}
+      <h1 className='text-center my-3 text-secondary'>Our Services</h1>
+      <Row xs={1} md={3} className='g-4'>
+        {services.map((service) => (
+          <Service key={service.id} service={service}></Service>
+        ))}
+      </Row>
     </div>
   );
 };
